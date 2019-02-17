14021adef0cf2fe6e01eab8a116a7ef571e4da3c
Brian Wheeler <brianwheeler@Brians-MacBook-Pro.local>
2019-02-17 06:29:51 -0700

    improve the landing page

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -7,26 +7,22 @@ export default class Splash extends Component {
     render() {
         return <div style={{minHeight: '100vh'}}>
             <div className='full-height d-flex flex-column pb-5'>
-                <div className='mb-5 d-flex justify-content-around align-items-center m-3'>
-                    <div className='w-25 d-flex justify-content-between'>
+                <div className='d-flex justify-content-around align-items-center m-3'>
+                    {/* <div className='w-25 d-flex justify-content-between'>
                         <a href='#about'>About</a>
                         <a></a>
-                    </div>
+                    </div> */}
                     <img className='w-25' style={{minWidth: '250px'}} src={logo} />
-                    <div className='w-25 d-flex justify-content-between'>
+                    {/* <div className='w-25 d-flex justify-content-between'>
                         <a onClick={this.signIn.bind(this)}>Connect to {process.env.REACT_APP_BURNER_NAME}</a>
-                    </div>
+                    </div> */}
                 </div>
-                <div className='d-flex justify-content-center flex-column h-100'>
-                    <div className='container'>
-                        <div className='row'>
-                            <div className='col-lg-6 offset-lg-3 col-md-12'>
-                                <div className='mt-5 text-center d-flex flex-grow flex-column justify-content-center'>
-                                    <h5>A seamless merchant experience for {process.env.REACT_APP_BURNER_NAME}</h5>
-                                    <div>
-                                        <Button color='primary' size='lg' onClick={this.signIn.bind(this)}>Connect with {process.env.REACT_APP_BURNER_NAME}</Button>
-                                    </div>
-                                </div>
+                <div className='d-flex justify-content-center flex-grow-1'>
+                    <div className='text-center d-flex flex-grow flex-column justify-content-center'>
+                        <div style={{maxWidth: '18em', margin: '0 auto'}} >
+                            <h5 className='mb-4'>A seamless merchant experience for {process.env.REACT_APP_BURNER_NAME}</h5>
+                            <div>
+                                <Button color='primary' size='lg' onClick={this.signIn.bind(this)}>Connect with {process.env.REACT_APP_BURNER_NAME}</Button>
                             </div>
                         </div>
                     </div>
